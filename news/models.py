b8693558bee86de059ff89c8c55ce1a730f6849a
vkhomyn95 <vkhomyn95@gmail.com>
2018-06-21 17:58:20 +0300

    second version

--- news/models.py
+++ news/models.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    cardimg = models.ImageField(upload_to='news/', blank=True, null=True)
+    cardtitle = models.CharField(max_length=200)
+    cardtext = models.TextField()
+    carddescription = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cardtitle
--- news/views.py
+++ news/views.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from news.models import  News
 
 
 def home(request):
-    return render(request, 'main.html', {})
+    news = News.objects.all()
+    return render(request, 'main.html', {'news': news})
